55d14d7ce67bac5b45256ad25d997bf531683931
Federico Perez <0xfede07c8@gmail.com>
2023-04-09 16:15:45 -0300

    version que funciona

--- crypto_conversion.asm
+++ crypto_conversion.asm
@@ -1,49 +1,14 @@
-; Define las variables para almacenar las tasas de conversión
-usd_to_ars_conversion_rate dd 96.50
-usd_to_eur_conversion_rate dd 0.83
-
-; Define las funciones que convierten de una moneda a otra
-global convert_to_ars
-global convert_to_usd
-global convert_to_eur
+global convert
 
 section .text
 
-; Función que convierte de USD a ARS
-convert_to_ars:
+; Funcion de conversion
+convert:
    push   ebp
    mov    ebp,esp
    sub    esp,0x4
    fld    dword  [ebp+0xc]
    fld    dword  [ebp+0x8]
-   fmulp st1, st0
-   fstp   dword  [ebp-0x4]
-   fld    dword  [ebp-0x4]
+   fmulp  st1, st0
    leave
    ret
-
-; Función que convierte de USD a USD (esto es inútil, pero está aquí como ejemplo)
-convert_to_usd:
-    ; Cargar el valor de USD a convertir en el coprocesador de punto flotante
-    fld dword [esp+4]
-    ; Cargar 1.0 en el coprocesador de punto flotante
-    fld1
-    ; Dividir los valores en el coprocesador de punto flotante
-    fdiv dword [esp+4]
-    ; Almacenar el resultado en la ubicación de memoria correspondiente en la pila
-    fstp dword [esp+8]
-    ; Devolver el control al código que llamó a esta función
-    ret 8
-
-; Función que convierte de USD a EUR
-convert_to_eur:
-    ; Cargar el valor de USD a convertir en el coprocesador de punto flotante
-    fld dword [esp+4]
-    ; Cargar la tasa de conversión de USD a EUR en el coprocesador de punto flotante
-    fld dword [usd_to_eur_conversion_rate]
-    ; Multiplicar los valores en el coprocesador de punto flotante
-    fmul
-    ; Almacenar el resultado en la ubicación de memoria correspondiente en la pila
-    fstp dword [esp+8]
-    ; Devolver el control al código que llamó a esta función
-    ret 8
--- crypto_prices.py
+++ crypto_prices.py
@@ -1,27 +1,48 @@
+#!/bin/env python3
 import requests
+import subprocess
 
-# URL de la API para obtener el precio de Bitcoin en USD
-url_btc = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 
-# URL de la API para obtener el precio de Ethereum en USD
-url_eth = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+def get_prices():
+    # URL de la API para obtener el precio de Bitcoin en USD
+    url_btc = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 
-# Hacer una solicitud HTTP GET a la API para obtener los precios de Bitcoin y Ethereum
-response_btc = requests.get(url_btc)
-response_eth = requests.get(url_eth)
+    # URL de la API para obtener el precio de Ethereum en USD
+    url_eth = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
 
-# Verificar si la solicitud fue exitosa
-if response_btc.status_code == 200 and response_eth.status_code == 200:
-    # Obtener el precio de Bitcoin en USD del JSON de la respuesta
-    btc_price = response_btc.json()["bitcoin"]["usd"]
-    
-    # Obtener el precio de Ethereum en USD del JSON de la respuesta
-    eth_price = response_eth.json()["ethereum"]["usd"]
+    # Hacer una solicitud HTTP GET a la API para obtener los precios de Bitcoin y Ethereum
+    response_btc = requests.get(url_btc)
+    response_eth = requests.get(url_eth)
 
-    # Guardar los precios de Bitcoin y Ethereum en un archivo de texto
-    with open("crypto_prices.txt", "w") as f:
-        f.write(str(btc_price) + "\n") # Escribir el precio de Bitcoin en la primera línea del archivo
-        f.write(str(eth_price) + "\n") # Escribir el precio de Ethereum en la segunda línea del archivo
-else:
-    # Si la solicitud no fue exitosa, imprimir un mensaje de error
-    print("Error in API request")
+    if response_btc.status_code == 200 and response_eth.status_code == 200:
+        # Obtener el precio de Bitcoin en USD del JSON de la respuesta
+        btc_price = response_btc.json()["bitcoin"]["usd"]
+        
+        # Obtener el precio de Ethereum en USD del JSON de la respuesta
+        eth_price = response_eth.json()["ethereum"]["usd"]
+
+        return btc_price, eth_price
+
+    return None, None
+
+def main(argv):
+    # Obtener los precios de Bitcoin y Ethereum
+    print('Pidiendo precios...')
+
+    btc_price, eth_price = get_prices()
+    if btc_price and eth_price:
+        print('Precios obtenidos!')
+        print("Bitcoin price USD: $%.2f" % btc_price)
+        print("Ethereum price USD: $%.2f" % eth_price)
+
+        subprocess.run(['./main', str(eth_price), str(btc_price)])
+    else:
+        print('Error al obtener los precios')
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+    exit(main(sys.argv))
